9e24396328ca25a1e0fa4373e881d1fb74c2296e
ercsssn <ercsssn@gmail.com>
2022-06-21 22:19:28 +0800

    Create core hash logic

--- script.rb
+++ script.rb
@@ -1,7 +1,12 @@
 def caesar_cipher(string)
     upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("") #TODO: Make this a hash
     lower_case = "abcdefghijklmnopqrstuvwxyz".split("")
+    lowercase_hash = Hash.new
     encrypted_string = ""
+
+    lower_case.each_with_index do |letter,index|
+        lowercase_hash[letter] = index
+    end
 
     string.split("").each do |letter| 
         if upper_case.include?(letter)
