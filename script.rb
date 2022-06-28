a71304cde0d5dde04b3d1acc86547596a5832bf4
John Ericsson <ercsssn@gmail.com>
2022-06-28 17:41:27 +0800

    Create the uppercase hash loop

--- script.rb
+++ script.rb
@@ -1,21 +1,26 @@
 def caesar_cipher(string)
-    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("") #TODO: Make this a hash
+    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
     lower_case = "abcdefghijklmnopqrstuvwxyz".split("")
-    lowercase_hash = Hash.new
+    upper_case_hash = Hash.new
+    lower_case_hash = Hash.new
     encrypted_string = ""
 
     lower_case.each_with_index do |letter,index|
-        lowercase_hash[letter] = index
+        lower_case_hash[letter] = index
     end
 
-    string.split("").each do |letter| 
-        if upper_case.include?(letter)
-            puts "I'm uppercase"
-        elsif lower_case.include?(letter)
-            puts "I'm lowercase"
-        else
-            puts "I'm a special character or a number"
-        end
+    upper_case.each_with_index do |letter,index|
+        upper_case_hash[letter] = index
     end
+
+    # string.split("").each do |letter| 
+    #     if upper_case.include?(letter)
+    #         puts "I'm uppercase"
+    #     elsif lower_case.include?(letter)
+    #         puts "I'm lowercase"
+    #     else
+    #         puts "I'm a special character or a number"
+    #     end
+    # end
 
 end
